58e0bb2628e0ea33d67393ac48c30b8cb703904d
hyeongjook <hyeongjoo1008@gmail.com>
2024-11-18 15:59:29 +0900

    main

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import Courseitem from './components/course/Courseitem';
+import CourseListCard from './components/course/CourseListCard';
+
+/* function Header() {
+  return(
+    <header>
+      <h2>Header</h2>
+    </header>
+  );
+}
+
+function Footer() {
+  return(
+    <footer>
+      <h2>Footer</h2>
+    </footer>
+  );
+}
+function Main() {
+  return(
+    <main>
+      <Counter />
+    </main>
+  );
+}
+
+// 파스칼 케이스 사용해야 된다. (첫글자 대문자 단어 첫글자 대문자)
+function Counter() {
+  return(
+    <button>Counter</button>
+  );
+} */
+
+// 리액트에서는 class는 예약어이기 때문에 사용을 할 수 없고, 
+// class 대신 className=''이라고 사용한다.
+function App() {
+  return (
+    <main>
+        <CourseListCard />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/course/Courseitem.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+function Courseitem() {
+	return (
+		<div>
+			<div class="course">
+				<img class="course__img" src="./img/coffee-blue.jpg" alt="" />
+				<div class="course__body">
+					<div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
+					<div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Courseitem;
